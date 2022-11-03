70c85eb5f985242b7e05d34c05a82a3c15e3279a
professor-oats <chronophob@gmail.com>
2022-11-03 17:56:49 +0100

    adding camera to the scene

--- rtweekend.h
+++ rtweekend.h
@@ -5,6 +5,8 @@
 #include <limits>
 #include <memory>
 
+#include <random>
+
 // Usings
 
 using std::shared_ptr;
@@ -20,6 +22,18 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
   return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+  static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+  static std::mt19937 generator;
+  return distribution(generator);
+}
+
+inline double clamp(double x, double min, double max) {
+  if (x < min) return min;
+  if (x > max) return max;
+  return x;
 }
 
 // Common Headers
