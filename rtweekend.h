6f5ec6c89f93619a47305cc532e92a8fca9cbb04
professor-oats <chronophob@gmail.com>
2022-11-03 19:05:43 +0100

    adding lambertian

--- rtweekend.h
+++ rtweekend.h
@@ -30,6 +30,11 @@ inline double random_double() {
   return distribution(generator);
 }
 
+inline double random_double(double min, double max) {
+  // Returns a random real in [min, max).
+  return min + (max-min)*random_double();
+}
+
 inline double clamp(double x, double min, double max) {
   if (x < min) return min;
   if (x > max) return max;
