9f8016eb12e76a0f13c2bb07b8fe889c09dbc5f3
professor-oats <chronophob@gmail.com>
2023-01-24 14:03:09 +0100

    moving_spheres_and_time

--- camera.h
+++ camera.h
@@ -12,7 +12,9 @@ class camera {
       double vfov,  // vertical field-of-view in degrees
       double aspect_ratio,
       double aperture,
-      double focus_dist
+      double focus_dist,
+      double _time0 = 0,
+      double _time1 = 0
     )  {
       
       auto theta = degrees_to_radians(vfov);
@@ -31,13 +33,20 @@ class camera {
       vertical = focus_dist * viewport_height * v;
       lower_left_corner = origin - horizontal/2 - vertical/2 - focus_dist*w;
       lens_radius = aperture / 2;
+      time0 = _time0;
+      time1 = _time1;
     }
     
     ray get_ray(double s, double t) const {
       vec3 rd = lens_radius * random_in_unit_disk();
       vec3 offset = u * rd.x() + v * rd.y();
 
-      return ray(origin + offset, lower_left_corner + s*horizontal + t*vertical - origin - offset);
+      return ray(
+        origin + offset,
+        lower_left_corner + s*horizontal + t*vertical - origin - offset,
+        random_double(time0, time1)
+            );
+
     }
 
   private:
@@ -47,5 +56,6 @@ class camera {
     vec3 vertical;
     vec3 u, v, w;
     double lens_radius;
+    double time0, time1;
 };
 #endif
