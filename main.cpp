069b9140809af9b21bec3880d2c2f300397c2e77
professor-oats <chronophob@gmail.com>
2023-04-04 10:43:45 +0200

    earth

--- main.cpp
+++ main.cpp
@@ -106,6 +106,14 @@ hittable_list two_perlin_spheres() {
 	return objects;
 }
 
+hittable_list earth() {
+  auto earth_texture = make_shared<image_texture>("earthmap.jpg");
+  auto earth_surface = make_shared<lambertian>(earth_texture);
+  auto globe = make_shared<sphere>(point3(0,0,0), 2, earth_surface);
+
+  return hittable_list(globe);
+}
+
 int main() {
   // Image
 
@@ -139,13 +147,21 @@ switch (0) {
         vfov = 20.0;
         break;
 
-    default:
 	case 3:
 		world = two_perlin_spheres();
 		lookfrom = point3(13,2,3);
 		lookat = point3(0,0,0);
 		vfov = 20.0;
 		break;
+
+    default:
+    case 4:
+	  world = earth();
+	  lookfrom = point3(13,2,3);
+	  lookat = point3(0,0,0);
+	  vfov = 20.0;
+	  break;
+
 }
 
 // Camera
